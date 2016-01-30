dfeb53c2b6a99f6bdca62b320f3c1e7740785415
IndieForger <indieforger@gmail.com>
2016-01-30 17:04:26 +0000

    social icons

--- demo/index.js
+++ demo/index.js
@@ -28,7 +28,27 @@ const config = {
 				{ label: 'contact', url: '/contact' },
 				{ label: 'project-demo', url: '/project-demo' }
 			]
-		}
+		},
+		// font awesome based icons
+		social: [
+			{ channel: 'twitter', url: 'https://twitter.com/IndieForger' },
+			{ channel: 'tumblr', url: 'https://www.tumblr.com/blog/indieforger' },
+			{ channel: 'youtube', url: 'https://www.youtube.com/channel/UCWPbP_gp1lUk46wMiIGoXAQ' },
+			{ channel: 'github', url: 'https://github.com/indieforger' },
+			{ channel: 'google-plus', url: 'https://plus.google.com/b/103153982100921182774/103153982100921182774' },
+			{ channel: 'facebook', url: 'https://www.facebook.com/indieforger' },
+			/*
+			{ channel: 'steam', url: '' }
+			{ channel: 'reddit', url: '' }
+			{ channel: 'android', url: '' }
+			{ channel: 'apple', url: '' }
+			{ channel: 'instagram', url: '' }
+			{ channel: 'linkedin', url: '' }
+			{ channel: 'send', url: '' }
+			{ channel: 'rss', url: '' }
+			 */
+
+		]
 	}
 }
 
