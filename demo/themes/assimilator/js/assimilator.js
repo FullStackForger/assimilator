e5c670312008737b76690e1e387247173b95628b
IndieForger <indieforger@gmail.com>
2016-01-31 13:19:56 +0000

    theme navbar narrowed

--- demo/themes/assimilator/js/assimilator.js
+++ demo/themes/assimilator/js/assimilator.js
@@ -1,6 +1,6 @@
 // Navigation Scripts to Show Header on Scroll-Up
 $(document).ready(function($) {
-	var MQL = 1170;
+	var MQL = 768;
 
 	//primary navigation slide-in effect
 	if ($(window).width() > MQL) {
