f40dfcafeb88a9b3bba47f6d03e89b56f7d19e99
IndieForger <indieforger@gmail.com>
2016-04-17 18:07:38 +0100

    template element filter now returns null value if used incorrectly

--- demo/themes/assimilator/helpers/element.js
+++ demo/themes/assimilator/helpers/element.js
@@ -1,31 +1,28 @@
-'use strict'
+'use strict';
 // usage:
 // {{element array objKey objValue}}
 // will return first object from array where obj[objKey] equals objValue
-
+// will return null if element cant be selected
 var element = module.exports = function() {
 
-	let args = Array.prototype.slice.call(arguments)
-	if (args.length != 4) {
-		throw new Error('Handlebar Element helper requires 3 parameters: array, key, value')
-	}
+	let args = Array.prototype.slice.call(arguments);
 
-	let array = args[0]
-	let key = args[1]
-	let value = args[2]
-	let context = args[3]
-	let found = false
-	let index = 0
+	let array = args[0];
+	let key = args[1];
+	let value = args[2];
+	let context = args[3];
+	let found = false;
+	let index = 0;
 
-	while(!found && index < array.length) {
-		let obj = array[index]
+	while(!found && array && index < array.length) {
+		let obj = array[index];
 		if (obj.hasOwnProperty(key) && obj[key] === value) {
 			return obj
 		}
 		index++
 	}
-	return null
+	return null;
 	//return JSON.stringify(args, null, 2);
-}
+};
 
 module.exports = element;
