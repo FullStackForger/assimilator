6171690501e2c38c58c113b14026595c6d5557b3
IndieForger <indieforger@gmail.com>
2016-04-18 09:21:34 +0100

    migration demos

--- demo/index-multisite.js
+++ demo/index-multisite.js
@@ -1,28 +1,17 @@
-'use strict'
-const Assimilator = require('../')
-const Hoek = require('hoek')
-const Forger = require('forger')
-const defaults = {
-	server: {
-		env: "development",
-		host: "localhost",
-		port: 8080
-	},
-	settings: {
-		globals: {
-			path: __dirname
+'use strict';
+const Assimilator = require('../');
+const config = {
+	env: 'development',
+	site: {
+		rootPath: __dirname,
+		series: {
+			listAsCategory: false,
+			longTitle: true,
+			longTitleDelimiter: ' '
 		},
 		theme: {
 			path: './themes/assimilator',
 			layout: 'default'
-		},
-		blog: {
-			path: './blog.second-site.com/',
-			series: {
-				listAsCategory: false,
-				longTitle: true,
-				longTitleDelimiter: ' '
-			}
 		},
 		pages: {
 			path: './pages/'
@@ -30,54 +19,49 @@ const defaults = {
 		files: {
 			index: ['index.html', 'index.htm'],
 			path: './projects/'
+		},
+		context: {
+			title: 'My personal site',
+			menu: {
+				main: [
+					{ label: 'about', url: '/about' },
+					{ label: 'contact', url: '/contact' },
+					{ label: 'project-demo', url: '/project-demo' }
+				]
+			},
+			// font awesome based icons
+			social: [
+				{ channel: 'twitter', url: 'https://twitter.com/IndieForger' },
+				{ channel: 'tumblr', url: 'https://www.tumblr.com/blog/indieforger' },
+				{ channel: 'youtube', url: 'https://www.youtube.com/channel/UCWPbP_gp1lUk46wMiIGoXAQ' },
+				{ channel: 'github', url: 'https://github.com/indieforger' },
+				{ channel: 'google-plus', url: 'https://plus.google.com/b/103153982100921182774/103153982100921182774' },
+				{ channel: 'facebook', url: 'https://www.facebook.com/indieforger' }
+			]
 		}
 	},
-	context: {
-		title: 'My personal site',
-		menu: {
-			main: [
-				{ label: 'about', url: '/about' },
-				{ label: 'contact', url: '/contact' },
-				{ label: 'project-demo', url: '/project-demo' }
-			]
-		},
-		// font awesome based icons
-		social: [
-			{ channel: 'twitter', url: 'https://twitter.com/IndieForger' },
-			{ channel: 'tumblr', url: 'https://www.tumblr.com/blog/indieforger' },
-			{ channel: 'youtube', url: 'https://www.youtube.com/channel/UCWPbP_gp1lUk46wMiIGoXAQ' },
-			{ channel: 'github', url: 'https://github.com/indieforger' },
-			{ channel: 'google-plus', url: 'https://plus.google.com/b/103153982100921182774/103153982100921182774' },
-			{ channel: 'facebook', url: 'https://www.facebook.com/indieforger' }
-		]
-	}
-}
+	sites: [{
+		connection: { host: 'localhost', port: 9001, name: 'fist-site' },
+		path: './blog.first-site.com/',
+		context: {
+			title: 'First personal site'
+		}
+	},{
+		connection: { host: 'localhost', port: 9002, name: 'second-site' },
+		path: './blog.second-site.com/',
+		context: {
+			title: 'Second personal site'
+		}
+	}]
+};
 
-const config1 = Hoek.clone(defaults)
-config1.server.port = 9001
-config1.settings.blog.path = './blog.first-site.com/'
-config1.context.title =  'First personal site'
-
-const config2 = Hoek.clone(defaults)
-config2.server.port = 9002
-config2.settings.blog.path = './blog.second-site.com/'
-config2.context.title =  'Second personal site'
-
-function startServer(config, next) {
-	new Assimilator
-		.Server(config)
-		.start().then((server) => {
-			console.log('Server started at: ' + server.info.uri + ' SUCCESS')
-			next()
-		}).catch((err) => {
-			next(err)
-		})
-}
-
-Forger
-	.sequence(
-		(next) => startServer(config1, next),
-		(next) => startServer(config2, next)
-	)
-	.then(() => console.log('All servers started'))
-	.catch((error) => console.error(error))
+Assimilator
+	.start(config)
+	.then((server) => {
+		server.connections.forEach((connection) => {
+			console.log(`Server started at: ${connection.info.uri}`);
+		});
+	})
+	.catch((error) => {
+		console.error(error.stack);
+	});
--- demo/index-single.js
+++ demo/index-single.js
@@ -1,26 +1,18 @@
-'use strict'
-const Assimilator = require('../')
+'use strict';
+const Assimilator = require('../');
 const config = {
-	server: {
-		env: "development",
-		host: "localhost",
-		port: 8080
-	},
-	settings: {
-		globals: {
-			path: __dirname
+	env: 'development',
+	site: {
+		connection: {
+			host: 'localhost',
+			port: 8080,
+			name: 'default'
 		},
+		rootPath: __dirname,
+		path: './blog.first-site.com',
 		theme: {
 			path: './themes/assimilator',
 			layout: 'default'
-		},
-		blog: {
-			path: './blog.first-site.com/',
-			series: {
-				listAsCategory: false,
-				longTitle: true,
-				longTitleDelimiter: ' '
-			}
 		},
 		pages: {
 			path: './pages/'
@@ -28,43 +20,40 @@ const config = {
 		files: {
 			index: ['index.html', 'index.htm'],
 			path: './projects/'
-		}
-	},
-	context: {
-		title: 'My personal site',
-		menu: {
-			main: [
-				{ label: 'about', url: '/about' },
-				{ label: 'contact', url: '/contact' },
-				{ label: 'project-demo', url: '/project-demo' }
-			]
 		},
-		// font awesome based icons
-		social: [
-			{ channel: 'twitter', url: 'https://twitter.com/IndieForger' },
-			{ channel: 'tumblr', url: 'https://www.tumblr.com/blog/indieforger' },
-			{ channel: 'youtube', url: 'https://www.youtube.com/channel/UCWPbP_gp1lUk46wMiIGoXAQ' },
-			{ channel: 'github', url: 'https://github.com/indieforger' },
-			{ channel: 'google-plus', url: 'https://plus.google.com/b/103153982100921182774/103153982100921182774' },
-			{ channel: 'facebook', url: 'https://www.facebook.com/indieforger' },
-			/*
-			{ channel: 'steam', url: '' }
-			{ channel: 'reddit', url: '' }
-			{ channel: 'android', url: '' }
-			{ channel: 'apple', url: '' }
-			{ channel: 'instagram', url: '' }
-			{ channel: 'linkedin', url: '' }
-			{ channel: 'send', url: '' }
-			{ channel: 'rss', url: '' }
-			 */
-		]
+		series: {
+			listAsCategory: false,
+			longTitle: true,
+			longTitleDelimiter: ' '
+		},
+		context: {
+			title: 'My personal site',
+			menu: {
+				main: [
+					{label: 'about', url: '/about'},
+					{label: 'contact', url: '/contact'},
+					{label: 'project-demo', url: '/project-demo'}
+				]
+			},
+			// font awesome based icons
+			social: [
+				{channel: 'twitter', url: 'https://twitter.com/IndieForger'},
+				{channel: 'tumblr', url: 'https://www.tumblr.com/blog/indieforger'},
+				{channel: 'youtube', url: 'https://www.youtube.com/channel/UCWPbP_gp1lUk46wMiIGoXAQ'},
+				{channel: 'github', url: 'https://github.com/indieforger'},
+				{channel: 'google-plus', url: 'https://plus.google.com/b/103153982100921182774/103153982100921182774'},
+				{channel: 'facebook', url: 'https://www.facebook.com/indieforger'}
+			]
+		}
 	}
-}
+};
 
-new Assimilator
-	.Server(config)
-	.start().then(() => {
-		//console.log('successful')
+Assimilator
+	.start(config)
+	.then((server) => {
+		server.connections.forEach((connection) => {
+			console.log(`Server started at: ${connection.info.uri}`);
+		});
 	}).catch((err) => {
 		console.log(err)
-	})
+	});
